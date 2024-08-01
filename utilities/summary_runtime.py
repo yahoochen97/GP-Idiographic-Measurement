298418f30d9b7d275d3e62f37f289046fec8f6e0
Yehu Chen <chenyehu@wustl.edu>
2024-07-31 23:04:44 -0500

    runtime

--- utilities/summary_runtime.py
+++ utilities/summary_runtime.py
@@ -1,5 +1,6 @@
 # loading packages
 import numpy as np
+import pandas as pd
 
 PREFIX = "n10m20t30seed"
 MODEL = ["f2both_prior", "r5graded_multi", "f2pop", "r5ind",
@@ -18,8 +19,14 @@ for SEED in range(1, MAX_SEED + 1):
         with open(loading_file) as f:
             data = f.readlines()
             data = data[-5]
-            data = data.split(":")[1].replace(" ", "")
-            print(data)
+            data = int(data.split(":")[1].replace(" ", "")[:-4])
+            results[i, SEED - 1] = data
+
+results_mu = np.round(np.mean(results, axis=1), decimals=3)
+results_std = np.round(np.std(results, axis=1) / np.sqrt(MAX_SEED), decimals=3)
+
+results = pd.DataFrame({"model": MODEL, "avg runtime": results_mu, "std runtime": results_std})
+print(results)
 
         
 
