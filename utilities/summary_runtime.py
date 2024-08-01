d4547ae3ecf6f84b959fb4c206c0249b165e159f
Yehu Chen <chenyehu@wustl.edu>
2024-07-31 23:00:56 -0500

    runtime

--- utilities/summary_runtime.py
+++ utilities/summary_runtime.py
@@ -1,6 +1,5 @@
 # loading packages
 import numpy as np
-import pandas as pd
 
 PREFIX = "n10m20t30seed"
 MODEL = ["f2both_prior", "r5graded_multi", "f2pop", "r5ind",
@@ -14,7 +13,7 @@ results = np.zeros((len(MODEL), MAX_SEED))
 
 for SEED in range(1, MAX_SEED + 1):
     for i in range(len(MODEL)):
-        loading_file = "{}{}{}.log".format(PREFIX, SEED, MODEL[i])
+        loading_file = RESULT_PATH + "{}{}{}.log".format(PREFIX, SEED, MODEL[i])
 
         with open(loading_file) as f:
             data = f.readlines()
